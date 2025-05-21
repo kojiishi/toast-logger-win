ee556c5715b33a6e6afe714236c99887eec44789
Koji Ishii <kojii@chromium.org>
2025-05-22 03:12:20 +0900

    internal: Move Windows API structs to the `win` module (#6)

--- src/win/mod.rs
+++ src/win/mod.rs
@@ -1,4 +1,4 @@
-mod win {
+mod winapi {
     pub use windows::UI::Notifications::{
         ToastNotification, ToastNotificationManager, ToastNotifier, ToastTemplateType,
     };
@@ -6,23 +6,23 @@ mod win {
 
 #[derive(Debug)]
 pub struct ToastNotification {
-    notification: win::ToastNotification,
+    notification: winapi::ToastNotification,
 }
 
 impl ToastNotification {
     pub fn new_with_text(text: &str) -> anyhow::Result<Self> {
-        let template = win::ToastTemplateType::ToastText01;
-        let toast_xml = win::ToastNotificationManager::GetTemplateContent(template)?;
+        let template = winapi::ToastTemplateType::ToastText01;
+        let toast_xml = winapi::ToastNotificationManager::GetTemplateContent(template)?;
         let text_node = toast_xml.SelectSingleNode(&"//text[@id=\"1\"]".into())?;
         text_node.SetInnerText(&text.into())?;
-        let notification = win::ToastNotification::CreateToastNotification(&toast_xml)?;
+        let notification = winapi::ToastNotification::CreateToastNotification(&toast_xml)?;
         Ok(Self { notification })
     }
 }
 
 #[derive(Debug)]
 pub struct ToastNotifier {
-    notifier: win::ToastNotifier,
+    notifier: winapi::ToastNotifier,
 }
 
 impl ToastNotifier {
@@ -31,7 +31,7 @@ impl ToastNotifier {
         r"{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe";
 
     pub fn new_with_application_id(application_id: &str) -> anyhow::Result<Self> {
-        let manager = win::ToastNotificationManager::GetDefault()?;
+        let manager = winapi::ToastNotificationManager::GetDefault()?;
         let notifier = manager.CreateToastNotifierWithId(&application_id.into())?;
         Ok(Self { notifier })
     }
