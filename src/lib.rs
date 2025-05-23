d8d0773681253bd7be30c61d1c044463d70a85ee
Koji Ishii <kojii@chromium.org>
2025-05-24 03:55:40 +0900

    Change the `win` module to public (#7)

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 //! ```
 //! [Windows Toast Notifications]: https://learn.microsoft.com/windows/apps/design/shell/tiles-and-notifications/toast-notifications-overview
 
-pub(crate) mod win;
+pub mod win;
 
 mod toast_logger;
 pub use toast_logger::*;
