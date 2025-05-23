232fc3fe0dd441f27789c5e659aeb924a0c96435
Koji Ishii <kojii@chromium.org>
2025-05-24 03:50:28 +0900

    Change the `win` module to public

--- src/win/mod.rs
+++ src/win/mod.rs
@@ -8,16 +8,29 @@ mod winapi {
     };
 }
 
+/// Represents a Toast Notification.
+///
 /// A thin wrapper for the [`windows::UI::Notifications::ToastNotification`].
 ///
+/// Please also see the [`Windows.UI.Notifications.ToastNotification` class].
+///
 /// [`windows::UI::Notifications::ToastNotification`]: https://microsoft.github.io/windows-docs-rs/doc/windows/UI/Notifications/struct.ToastNotification.html
+/// [`Windows.UI.Notifications.ToastNotification` class]: https://learn.microsoft.com/uwp/api/windows.ui.notifications.toastnotification
 #[derive(Debug)]
 pub struct ToastNotification {
     notification: winapi::ToastNotification,
 }
 
 impl ToastNotification {
-    /// Create a `ToastNotification` with the given text.
+    /// Create a `win::ToastNotification` with the given text.
+    /// # Examples
+    /// ```
+    /// # use toast_logger_win::win::{ToastNotification, ToastNotifier};
+    /// fn show_text(notifier: &ToastNotifier, text: &str) -> anyhow::Result<()> {
+    ///     let notification = ToastNotification::new_with_text(text)?;
+    ///     notifier.show(&notification)
+    /// }
+    /// ```
     pub fn new_with_text(text: &str) -> anyhow::Result<Self> {
         let template = winapi::ToastTemplateType::ToastText01;
         let toast_xml = winapi::ToastNotificationManager::GetTemplateContent(template)?;
@@ -30,7 +43,10 @@ impl ToastNotification {
 
 /// A thin wrapper for the [`windows::UI::Notifications::ToastNotifier`].
 ///
+/// Please also see the [`Windows.UI.Notifications.ToastNotifier` class].
+///
 /// [`windows::UI::Notifications::ToastNotifier`]: https://microsoft.github.io/windows-docs-rs/doc/windows/UI/Notifications/struct.ToastNotifier.html
+/// [`Windows.UI.Notifications.ToastNotifier` class]: https://learn.microsoft.com/uwp/api/windows.ui.notifications.toastnotifier
 #[derive(Debug)]
 pub struct ToastNotifier {
     notifier: winapi::ToastNotifier,
