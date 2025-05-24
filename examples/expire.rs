a959ea0597ff20ea1e1e2c3cc848cab01cd66f1d
Koji Ishii <kojii@chromium.org>
2025-05-25 02:35:19 +0900

    Cleanup examples/expire.rs

--- examples/expire.rs
+++ examples/expire.rs
@@ -4,18 +4,17 @@ use toast_logger_win::ToastLogger;
 
 pub fn main() -> anyhow::Result<()> {
     let mut builder = ToastLogger::builder();
-    #[allow(unused_assignments)]
-    #[allow(unused_mut)]
-    let mut message: String = "This message doesn't expire".into();
     #[cfg(feature = "winrt-toast")]
-    {
+    let message = {
         builder.create_notification(|records| {
             let mut notification = Notification::new_with_records(records)?;
             notification.expires_in(std::time::Duration::from_secs(3));
             Ok(notification)
         });
-        message = "This message should expire in 3 seconds".into();
-    }
+        "This message should expire in 3 seconds"
+    };
+    #[cfg(not(feature = "winrt-toast"))]
+    let message = "This message shouldn't expire";
     builder.max_level(log::LevelFilter::Info).init()?;
 
     log::info!("{}", message);
